cb3119cbd3e7779f9ba2b1ce7f2d71ae29e39408
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-01-30 23:23:02 +0200

    Added setup.py for more useful testing

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Tic-Tac-Toe',
+      version='0.1',
+      description='Tic Tac Toe game',
+      author='Andrii V. Mishkovskyi',
+      author_email='contact@mishkovskyi.net',
+      packages=['ttt'])
