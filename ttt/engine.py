c3429d4963755a208f7004c44d982b289def341a
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-01-30 18:44:23 +0200

    Simple layout for the engine created, also some plugs coded.

---
+++ ttt/engine.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+
+"""The engine for the tic-tac-toe game"""
+
+
+class Victory(Exception):
+    pass
+
+
+class Engine(object):
+
+    values = {'X': 1, 'O': -1}
+
+    def __init__(self):
+        self.board = dict(((x, y), None) for x in xrange(3) for y in xrange(3))
+
+    def move(self):
+        """Make AI move"""
+        coord = None
+        self.mark(coord, 'X')
+
+    def mark(self, coord, value):
+        """Mark the coord and check for the winning position"""
+        self.board[coord] = value
+        self.check()
+
+    def check(self):
+        """Simple routine for checking if somebody has won"""
+        for x in xrange(3):
+            if abs(sum(self.board[(x, y)] for y in xrange(3))) == 3:
+                raise Victory
+        for y in xrange(3):
+            if abs(sum(self.board[(x, y)] for x in xrange(3))) == 3:
+                raise Victory
+        # Now, check the diagonals
+        if (abs(sum(self.board[(x, x)] for x in xrange(3))) == 3 or
+            abs(sum(self.board[(x, 2 - x)] for x in xrange(3))) == 3):
+            raise Victory
